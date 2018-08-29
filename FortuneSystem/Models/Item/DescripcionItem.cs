c12c7fd94c39b2e8bcf48b9dc0e4f545d09329e2
Karen <karen@fortunesp.com>
2018-08-29 11:15:30 -0700

    Creacion de model y Data Items

--- FortuneSystem/Models/Item/DescripcionItem.cs
+++ FortuneSystem/Models/Item/DescripcionItem.cs
@@ -7,5 +7,28 @@ namespace FortuneSystem.Models.DescripcionItem
 {
     public class DescripcionItem
     {
+        [Display(Name = "Id")]
+        public int IdItems { get; set; }
+
+        [Required(ErrorMessage = "Ingrese el estilo del Item.")]
+        [Display(Name = "Item")]
+        public string EstiloItem { get; set; }
+
+        [Required(ErrorMessage = "Ingrese la descripción del Item.")]
+        [Display(Name = "Color")]
+        public int IdColor { get; set; }
+
+        [Required(ErrorMessage = "Ingrese la cantidad total del Item.")]
+        [Display(Name = "QTY")]
+        public int Cantidad { get; set; }
+
+        [Required(ErrorMessage = "Ingrese el precio del Item.")]
+        [Display(Name = "Precio.")]
+        public float Precio { get; set; }
+
+        [Display(Name = "No. PO")]
+        [Column(Storage = "ID_PEDIDOS")]
+        public Pedido pedido = new Pedido();
+
     }
 }
--- FortuneSystem/Models/Item/DescripcionItemData.cs
+++ FortuneSystem/Models/Item/DescripcionItemData.cs
@@ -7,5 +7,27 @@ namespace FortuneSystem.Models.DescripcionItem
 {
     public class DescripcionItemData
     {
+        private Conexion conn = new Conexion();
+        private SqlCommand comando = new SqlCommand();
+        private SqlDataReader leer = null;
+
+        public void AgregarPO(Pedido ordenCompra)
+        {
+            comando.Connection = conn.AbrirConexion();
+            comando.CommandText = "AgregarPedido";
+            comando.CommandType = CommandType.StoredProcedure;
+
+            comando.Parameters.AddWithValue("@idPO", ordenCompra.PO);
+            comando.Parameters.AddWithValue("@idPOF", ordenCompra.VPO);
+            comando.Parameters.AddWithValue("@Customer", ordenCompra.Cliente);
+            comando.Parameters.AddWithValue("@CustomerF", ordenCompra.ClienteFinal);
+            comando.Parameters.AddWithValue("@datecancel", ordenCompra.FechaCancel);
+            comando.Parameters.AddWithValue("@datePO", ordenCompra.FechaOrden);
+            comando.Parameters.AddWithValue("@totUnid", ordenCompra.TotalUnidades);
+
+            comando.ExecuteNonQuery();
+            conn.CerrarConexion();
+
+        }
     }
 }
