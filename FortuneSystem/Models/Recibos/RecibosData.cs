a3ee6fa2e6f4a6c0578367af778cd3f63d448091
Melissa Sandoval <Melissa@fortunesp.com>
2018-08-30 16:53:22 -0700

    Traté

---
+++ FortuneSystem/Models/Recibos/RecibosData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FortuneSystem.Models.Recibos
+{
+    public class RecibosData
+    {
+        
+
+    }
+
+   
+
+}
